47e7384aec62e776bc4c76ea227629b951f3e5ba
Arthur <arthurhensilva18@gmail.com>
2024-04-23 19:41:48 -0300

    first commit

---
+++ src/pages/RepositoriePage/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RepositoriesPage() {
+  return <div>RepositoriesPage</div>;
+}
+
+export default RepositoriesPage;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,31 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 
-import MainPage from "./pages/Main";
+import MainPage from "./pages/MainPage";
+import RepositoriesPage from "./pages/RepositoriePage";
 
 export function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-    </Routes>
+    <Router>
+      <Route path="/repositories">
+        <RepositoriesPage />
+      </Route>
+      <Route>
+        <MainPage />
+      </Route>
+    </Router>
   );
 }
+
+// import React from "react";
+// import { Route, Routes } from "react-router-dom";
+
+// import MainPage from "./pages/MainPage";
+
+// export function AppRoutes() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<MainPage />} />
+//     </Routes>
+//   );
+// }
